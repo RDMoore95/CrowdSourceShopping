7563f684ea7c2d859b0bae0c1a7a40c7b49f222f
Adam Walker <walkeragv@gmail.com>
2020-05-04 15:18:12 -0700

    add argon

--- romanesco/romanesco-app/app/screens/stores/stores.tsx
+++ romanesco/romanesco-app/app/screens/stores/stores.tsx
@@ -1,15 +1,28 @@
 import React, { Component } from 'react';
-import { ActivityIndicator, FlatList, StyleSheet, Text, View, Button,  ScrollView } from 'react-native';
+import { ActivityIndicator,
+  FlatList, 
+  StyleSheet, 
+  Dimensions, 
+  ImageBackground,
+  View, 
+  ScrollView } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { useNavigation } from '@react-navigation/native';
 
+import { Block, Text, theme } from "galio-framework";
+import { Button } from "../../components";
+import { Images, argonTheme } from "../../constants";
+import { HeaderHeight } from "../../constants/utils";
+
+const { width, height } = Dimensions.get("screen");
+
+const thumbMeasure = (width - 48 - 32) / 3;
+
 // var url = "http://192.168.1.2.:5000/getStores/";
 var url = "http://flip1.engr.oregonstate.edu:5005/getStores/";
-// console.log(url);
 
 export default class Feed extends React.Component {
-  //navigation = useNavigation();
 
   constructor(props) {
     super(props);
@@ -51,7 +64,7 @@ export default class Feed extends React.Component {
     const { data, isLoading } = this.state;
     // const { data, isLoading } = this.state;
 
-    console.log(JSON.stringify("Print something"));
+    // console.log(JSON.stringify("Print something"));
     //console.log(JSON.stringify("On stores page"));    
     // console.log(JSON.stringify(response));
     //console.log(response);
@@ -59,51 +72,106 @@ export default class Feed extends React.Component {
 
     return (
 
-      <View style={styles.container}>
+      <Block flex style={styles.profile}>
+        <Block flex>
+          <ImageBackground
+            source={Images.ProfileBackground}
+            style={styles.profileContainer}
+            imageStyle={styles.profileBackground}
+          >
 
-        <ScrollView>    
-          <Text style={styles.title}>Your Favorite Stores:</Text>
+            <ScrollView
+              showsVerticalScrollIndicator={false}
+              style={{ width, marginTop: '25%' }}
+            >
 
-        {isLoading ? <ActivityIndicator/> : (
-          <FlatList
-            data={data}
-            keyExtractor={({ id }, index) => id}
-            renderItem={({ item }) => (
-              <Text>{item.name}, {item.location}</Text>
+          <Block flex style={styles.profileCard}>
+
+          <Text size={24} color={argonTheme.COLORS.TEXT}>Your Favorite Stores</Text>
+
+            {isLoading ? <ActivityIndicator/> : (
+              <FlatList
+                data={data}
+                keyExtractor={({ id }, index) => id}
+                renderItem={({ item }) => (
+                  <Text size={16} color="#32325D" style={{ marginTop: 10 }}>{item.name}, {item.location}</Text>
+                )}
+              />
             )}
-          />
-        )}
 
-        </ScrollView>
+            </Block>
+        
+            </ScrollView>
 
-      </View>
+          </ImageBackground>
+
+        </Block>
+
+      </Block>
+
     );
   } 
 
 
 }
 
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 24,
-    backgroundColor: '#eaeaea',
-    borderColor: "#20232a",
-    alignItems: 'center',
-    justifyContent: 'center',
+  profile: {
+    marginTop: Platform.OS === "android" ? -HeaderHeight : 0,
+    // marginBottom: -HeaderHeight * 2,
+    flex: 1
   },
-  title: {
-    marginTop: 16,
-    paddingVertical: 8,
-    borderWidth: 4,
-    borderColor: "#20232a",
-    borderRadius: 6,
-    backgroundColor: "#61dafb",
-    color: "#20232a",
-    textAlign: "center",
-    fontSize: 30,
-    fontWeight: "bold"
+  profileContainer: {
+    width: width,
+    height: height,
+    padding: 0,
+    zIndex: 1
+  },
+  profileBackground: {
+    width: width,
+    //height: height / 2
+  },
+  profileCard: {
+    // position: "relative",
+    padding: theme.SIZES.BASE,
+    marginHorizontal: theme.SIZES.BASE,
+    marginTop: 65,
+    borderTopLeftRadius: 6,
+    borderTopRightRadius: 6,
+    backgroundColor: theme.COLORS.WHITE,
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 0 },
+    shadowRadius: 8,
+    shadowOpacity: 0.2,
+    zIndex: 2
+  },
+  info: {
+    paddingHorizontal: 40
+  },
+  avatarContainer: {
+    position: "relative",
+    marginTop: -80
+  },
+  avatar: {
+    width: 124,
+    height: 124,
+    borderRadius: 62,
+    borderWidth: 0
+  },
+  nameInfo: {
+    marginTop: 35
+  },
+  divider: {
+    width: "90%",
+    borderWidth: 1,
+    borderColor: "#E9ECEF"
+  },
+  thumb: {
+    borderRadius: 4,
+    marginVertical: 4,
+    alignSelf: "center",
+    width: thumbMeasure,
+    height: thumbMeasure
   }
 });
-
-
