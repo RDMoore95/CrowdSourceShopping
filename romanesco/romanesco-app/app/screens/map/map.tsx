c85ce69c43258cb7a77b7dfd5d739e1a47b92d6b
Dan <rdmoore19@gmail.com>
2020-05-04 20:59:15 -0600

    map screen fix

--- romanesco/romanesco-app/app/screens/map/map.tsx
+++ romanesco/romanesco-app/app/screens/map/map.tsx
@@ -9,26 +9,26 @@ import { useNavigation } from '@react-navigation/native';
 export default class Map extends Component {
   //navigation = useNavigation();
 
-  constructor(props) {
-    super(props);
+  // constructor(props) {
+  //   super(props);
 
-    this.state = {
-      data: [],
-      isLoading: true
-    };
-  }
+  //   this.state = {
+  //     data: [],
+  //     isLoading: true
+  //   };
+  // }
 
-  componentDidMount() {
-    fetch('http://flip1.engr.oregonstate.edu:4545/map')
-      .then((response) => response.json())
-      .then((json) => {
-        this.setState( {data: json });
-      })
-      .catch((error) => console.error(error))
-      .finally(() => {
-        this.setState({ isLoading: false });
-      })
-  }
+  // componentDidMount() {
+  //   fetch('http://flip1.engr.oregonstate.edu:4545/map')
+  //     .then((response) => response.json())
+  //     .then((json) => {
+  //       this.setState( {data: json });
+  //     })
+  //     .catch((error) => console.error(error))
+  //     .finally(() => {
+  //       this.setState({ isLoading: false });
+  //     })
+  // }
 
 
     onChangeText = (key, val) => {
